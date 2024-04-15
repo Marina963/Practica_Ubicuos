44e0d5ba7e0b2f2a8915057e51918032387a59d4
IreneUc3m <100472108@alumnos.uc3m.es>
2024-04-15 16:37:48 +0200

    fix remove

--- www/app/script_remove.js
+++ www/app/script_remove.js
@@ -1,19 +1,22 @@
 let posicion = []
-const sensorABS = new AbsoluteOrientationSensor({ frequency: 1, referenceFrame: "device" });
+const sensorABS = new AbsoluteOrientationSensor({ frequency: 2, referenceFrame: "device" });
 let cont = 0;
 let item;
 let id = "";
 
 sensorABS.addEventListener("reading", () => {
-  
   posicion = sensorABS.quaternion;
   if (posicion[3] <= 0.1 && posicion[2] <= 0.1){
+    if (cont == 0){
+      navigator.vibrate(500);
+    }
     cont += 1;
     console.log("mirando abajo");
-    if (cont >=4){
+    if (cont >=6){
       navigator.vibrate(1000);
       item = document.getElementsByClassName("mostrar_producto");
       remove(item[0]);
+      cont = 0;
       sensorABS.stop();
       sensorAcc.stop();
     }
