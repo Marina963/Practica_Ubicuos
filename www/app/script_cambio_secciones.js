b6eb2e1677bf4c91f697b69df01bcc5b04bf50c7
marina <100472115@alumnos.uc3m.es>
2024-04-21 18:18:31 +0200

    Comentarios 2

--- www/app/script_cambio_secciones.js
+++ www/app/script_cambio_secciones.js
@@ -1,4 +1,4 @@
-
+/*---------------Script para cambiar entre seciones------------------------ */
 const footer_favoritos = document.querySelector("#footer_favoritos");
 const favoritos = document.querySelector("#favoritos");
 const footer_maniqui = document.querySelector("#footer_maniqui");
@@ -11,39 +11,50 @@ const footer_perfil = document.querySelector("#footer_perfil");
 const perfil = document.querySelector("#perfil");
 
 
-//Funciones para poder cambiar de paginas en la aplicación
+
+//Funciones para poder cambiar entre las ditintas seciones de la paguina
+//Cada sección es un elemento del footer
+
+//Función para cambiar a la sección de favoritos
 const act_pag_favoritos = () => {
+  //Se ocultan todas las secciones menos favorito
   favoritos.style.display = 'block';
   maniqui.style.display = 'none';
   armario.style.display = 'none';
   esc_ropa.style.display = 'none';
   perfil.style.display = 'none';
 
+  //Cambio de color en el footer de favoritos para que usuario sepa que se encuentra en esa sección
   footer_favoritos.style.backgroundColor = "#dbbde4";
   footer_maniqui.style.backgroundColor = "#EBDCF0";
   footer_armario.style.backgroundColor = "#EBDCF0";
   footer_esc_ropa.style.backgroundColor = "#EBDCF0";
   footer_perfil.style.backgroundColor = "#EBDCF0";
 
+  //Se detienen las dos camaras, se apagan los sensores del carrito y se quita el maniqui
   document.dispatchEvent(cambiar_sitio);
   detenerGrabacion();
   detenerGrabacion_ropa();
   quitarModelo();
 }
 
+//Función para cambiar a la sección de maniquí
 const act_pag_maniqui= () => {
+  //Se ocultan todas las secciones menos maniquí
   favoritos.style.display = 'none';
   maniqui.style.display = 'block';
   armario.style.display = 'none';
   esc_ropa.style.display = 'none';
   perfil.style.display = 'none';
   
+  //Cambio de color en el footer de maniquí para que usuario sepa que se encuentra en esa sección
   footer_favoritos.style.backgroundColor ="#EBDCF0";
   footer_maniqui.style.backgroundColor = "#dbbde4";
   footer_armario.style.backgroundColor = "#EBDCF0";
   footer_esc_ropa.style.backgroundColor = "#EBDCF0";
   footer_perfil.style.backgroundColor = "#EBDCF0";
 
+   //Se detienen las dos camaras, se apagan los sensores del carrito y crea el maniqui
   document.dispatchEvent(cambiar_sitio);
   detenerGrabacion();
   quitarModelo();
@@ -52,71 +63,69 @@ const act_pag_maniqui= () => {
 
 }
 
+//Función para cambiar a la sección de armario
 const act_pag_armario= () => {
+   //Se ocultan todas las secciones menos armario
   favoritos.style.display = 'none';
   maniqui.style.display = 'none';
   armario.style.display = 'block';
   esc_ropa.style.display = 'none';
   perfil.style.display = 'none';
 
+  //Cambio de color en el footer de armario para que usuario sepa que se encuentra en esa sección
   footer_favoritos.style.backgroundColor = "#EBDCF0";
   footer_maniqui.style.backgroundColor = "#EBDCF0";
   footer_armario.style.backgroundColor = "#dbbde4"
   footer_esc_ropa.style.backgroundColor = "#EBDCF0";
   footer_perfil.style.backgroundColor = "#EBDCF0";
 
+  //Se detienen las dos camaras y se quita el maniqui
   detenerGrabacion();
   detenerGrabacion_ropa();
   quitarModelo();
 }
+
+//Función para cambiar a la sección de scanner
 const act_pag_esc_ropa= () => {
+   //Se ocultan todas las secciones menos scanner
   favoritos.style.display = 'none';
   maniqui.style.display = 'none';
   armario.style.display = 'none';
   esc_ropa.style.display = 'block';
   perfil.style.display = 'none';
 
+  //Cambio de color en el footer del scanner para que usuario sepa que se encuentra en esa sección
   footer_favoritos.style.backgroundColor = "#EBDCF0";
   footer_maniqui.style.backgroundColor = "#EBDCF0";
   footer_armario.style.backgroundColor = "#EBDCF0";
   footer_esc_ropa.style.backgroundColor = "#dbbde4";
   footer_perfil.style.backgroundColor = "#EBDCF0";
 
+   //Se detienela camara del probador, se apagan los sensores del carrito, se quita el maniqui y se activa el scanner
   document.dispatchEvent(cambiar_sitio);
   detenerGrabacion_ropa();
   quitarModelo();
   iniciar_grabacion()
-  // Se inicia la grbacion y se busca el qr,
-  /*
-  iniciar_grabacion()
-  .then(valor_qr => {
-  if(valor_qr =="pago"){
-    socket.emit("LISTA_PAGO");
-  }else{
-    add(valor_qr);
-
-  }
   
-    act_pag_armario();
-  })
-  .catch(error => {
-    console.error("Error al iniciar la grabación:", error);
-  });
-  */
 }
+
+//Función para cambiar a la sección de perfil
 const act_pag_perfil= () => {
+   //Se ocultan todas las secciones menos perfil
   favoritos.style.display = 'none';
   maniqui.style.display = 'none';
   armario.style.display = 'none';
   esc_ropa.style.display = 'none';
   perfil.style.display = 'block';
 
+  //Cambio de color en el footer de perfil para que usuario sepa que se encuentra en esa sección
   footer_favoritos.style.backgroundColor = "#EBDCF0";
   footer_maniqui.style.backgroundColor = "#EBDCF0";
   footer_armario.style.backgroundColor = "#EBDCF0";
   footer_esc_ropa.style.backgroundColor = "#EBDCF0";
   footer_perfil.style.backgroundColor = "#dbbde4";
 
+  //Se detienen las dos camaras, se apagan los sensores del carritoy se quita el maniqui
   document.dispatchEvent(cambiar_sitio);
   detenerGrabacion();
   detenerGrabacion_ropa();
--- www/app/script_guia.js
+++ www/app/script_guia.js
@@ -1,3 +1,5 @@
+/*-------------Script con la guía de uso------------------- */
+
 const guia = document.querySelector("#guia");
 const app = document.querySelector("#app");
 const guia_navegacion = document.querySelector("#guia_navegacion");
@@ -10,12 +12,15 @@ const guia_pago = document.querySelector("#guia_pago");
 const siguiente = document.querySelector(".siguiente");
 const acceptar = document.querySelector("#acceptar");
 
+
+//Funión que ocualta la app y muestra la guía
 const inicio = () =>{
     app.style.display = "none";
     guia.style.display = "block";
     cambiar_navegación();
 }
 
+//Funciones que se encargan de mosntras las distintas páginas de la guía
 const cambiar_navegación = () =>{
     guia_navegacion.style.display = "block";
     guia_armario.style.display = "none";
@@ -80,6 +85,8 @@ const cambiar_pago = () =>{
     guia_pago.style.display = "block";
     siguiente.style.display = "none";
 }
+
+//Función que se encarga de pasar entre las páginas de la guía al pulsar siguiente
 siguiente.addEventListener("touchend", ()=>{
     if(guia_navegacion.style.display === "block"){
         cambiar_armario();
@@ -103,12 +110,15 @@ siguiente.addEventListener("touchend", ()=>{
 
 });
 
+
+//Función que cierra la guía y abre la app al pulsar la x
 cerrar.addEventListener("touchend", ()=>{
     app.style.display = "block";
     guia.style.display = "none";
     act_pag_armario();
 });
 
+//Función que cierra la guía y abre la app al pulsar acceptar en la última página
 acceptar.addEventListener("touchend", ()=>{
     app.style.display = "block";
     guia.style.display = "none";
